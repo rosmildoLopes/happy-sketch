1ff043c5444789cc4b075f2b101d4e1adf79927f
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-09-07 00:17:16 -0300

    get all element to the DOM

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ const colorBtn = document.getElementById('colorBtn')
 const rainbowBtn = document.getElementById('rainbowBtn')
 const eraserBtn = document.getElementById('eraserBtn')
 const clearBtn = document.getElementById('clearBtn')
-const colorPicker = document.getElementById('colorPicker')
+const rangeSlider = document.getElementById('rangeSlider')
+const board = document.getElementById('board')
+
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorBtn.onclick = () => setCurrentMode('color');
+rainbowBtn.onclick = () => setCurrentMode('rainbow');
+eraserBtn.onclick = () => setCurrentMode('eraser');
+clearBtn.onclick = () => reloadBoard();
+rangeSlider.onmousemove = (e) => updateRangeValue(e.target.value);
+rangeSlider.onchange = (e) => changeRange(e.target.value);
+
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
