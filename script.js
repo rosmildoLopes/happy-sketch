adb79ae1eef0cf2edeb202341be19ed677262b40
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-09-07 18:42:43 -0300

    setup funtions for the board usage

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const DEFAULT_MODE = "#color";
+const DEFAULT_COLOR = "#111111";
+const DEFAULT_SIZE = 16;
+
+let currentColor = DEFAULT_COLOR  
+let currentMode = DEFAULT_MODE  
+let currentSize = DEFAULT_SIZE  
+
+
+
 const colorPicker = document.getElementById('colorPicker')
 const colorBtn = document.getElementById('colorBtn')
 const rainbowBtn = document.getElementById('rainbowBtn')
@@ -19,3 +29,45 @@ rangeSlider.onchange = (e) => changeRange(e.target.value);
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
+
+function setCurrentMode(newMode) {
+  activateButton(newMode)
+  currentMode = newMode
+}
+
+function setCurrentColor(newColor) {
+  currentColor = newColor
+}
+
+function setCurrentSize(newSize) {
+  currentSize = newSize
+}
+
+function changeRange(value) {
+  updateRangeValue(value)
+  setCurrentSize(value)
+  reloadBoard()
+}
+
+function updateRangeValue() {
+  sizeValue.innerHTMl = `${value} x ${value}`
+}
+
+function reloadBoard() {
+  clearBoard()
+  setupBoard(currentSize)
+}
+
+function clearBoard() {
+  board.innerHTML = ''
+}
+
+function setupBoard(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+}
+window.onload = () => {
+  setupBoard(DEFAULT_SIZE)
+  activateButton(DEFAULT_MODE)
+}
+
